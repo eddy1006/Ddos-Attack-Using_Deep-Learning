ff249a0ed55904cc9426303c39ec7b2b3aec8f09
Archit Jain <arcyjain2002@gmail.com>
2022-11-08 20:34:39 +0530

    env update

--- smsService.py
+++ smsService.py
@@ -1,12 +1,15 @@
 import requests
 import base64
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-appId = "01GH72WYV283GVWHYW2VAZYBBR"
-accessKey = "b597f6be-6dbe-4a0a-859a-0d605259648e"
-accessSecret = "W~f5K5yF-lFBN_lk9.lu5Qja6R"
-projectId = "82ee0b94-e39f-42e5-bb3d-353a07588059"
+appId = os.getenv('APPID')
+accessKey = os.getenv('ACCESSKEY')
+accessSecret = os.getenv('ACCESSSECRET')
+projectId = os.getenv('PROJECTID')
 channel = "SMS"
-identity = "+918858893156"
+identity = "+918840194848"
 url = "https://us.conversation.api.sinch.com/v1/projects/" + projectId + "/messages:send"
 
 data = accessKey + ":" + accessSecret
@@ -27,7 +30,7 @@ payload = {
   },
   "message": {
       "text_message": {
-          "text": "Hello Mr. Pranav, we are looking for desperate people and who else comes to mind other than you. To make your life less miserable we are organising sperm donation camp in VIT near chancellor's office tommorrow @ 2 PM. You are a prime candidate so grab this one time chance you virgin!!! or die alone. \n We look forward for your sperm."
+          "text": "Ddos Attack Detected. Kindly take necessary action."
       }
   }  
 }
